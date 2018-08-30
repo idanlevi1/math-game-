93b607e3cde605c32f3d6f635998d58e3726494c
idanlevi <idanlevi2@gmail.com>
2018-08-30 17:44:50 +0300

    fix yellow warm bug (time)

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import GameApp from './src/App/GameApp'
+import _ from 'lodash';
 
 export default class App extends Component {
+  componentDidMount = () => {
+    console.ignoredYellowBox = ['Setting a timer'];
+    const _console = _.clone(console);
+    console.warn = message => {
+      if (message.indexOf('Setting a timer') <= -1) {
+        _console.warn(message);
+      }
+    };
+  };
+  
   render() {
     return (
       <GameApp/>
